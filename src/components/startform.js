1175ad1ccff2b251931a408d4214b6f8a7cc59ff
Anton Takov CW <A54H0ZZ@mmm.com>
2021-10-01 12:43:03 +0300

    added qrcode invitation

--- src/components/startform.js
+++ src/components/startform.js
@@ -5,12 +5,14 @@ export default function StartForm({ User, Color, RoomID, SetColorUser, IsDisable
     const [lroomid, setLroomid] = useState();
     const [isDisabled, setisDisabled] = useState();
     const [url, seturl] = useState();
+    const [qrCode, setQrCode] = useState("");
     useEffect(() => {
         setLcolor(Color);
         setLuser(User);
         setLroomid(RoomID);
         setisDisabled(IsDisabled)
         seturl(window.location.href + "?user=" + User + "&color=" + Color + "&room=" + RoomID)
+        setQrCode(`http://api.qrserver.com/v1/create-qr-code/?data=${url}!&size=150x150`);
     }, [User, Color, RoomID, IsDisabled])
     const handleChange=(event)=>{
         const value =  event.target.value;
@@ -20,23 +22,33 @@ export default function StartForm({ User, Color, RoomID, SetColorUser, IsDisable
             setLuser(value)
             seturl(window.location.href + "?user=" + value + "&color=" + lcolor + "&room=" + lroomid)
             SetColorUser(value, lcolor)
+            localStorage.setItem(
+                'userinfo', JSON.stringify({
+                    username: value,
+                    color: lcolor,
+                }));
         }
         else {
             setLcolor(value)
             seturl(window.location.href + "?user=" + luser + "&color=" + value + "&room=" + lroomid)
             SetColorUser(luser, value)
+            localStorage.setItem(
+                'userinfo', JSON.stringify({
+                    username: luser,
+                    color: value,
+                }));
         }
  
     }
 
-    const handleSubmit=(event) => {
-        event.preventDefault();
-        localStorage.setItem(
-            'userinfo', JSON.stringify({
-                username: luser,
-                color: lcolor,
-            }));
-    }
+    //const handleSubmit=(event) => {
+    //    event.preventDefault();
+    //    localStorage.setItem(
+    //        'userinfo', JSON.stringify({
+    //            username: luser,
+    //            color: lcolor,
+    //        }));
+    //}
     
 
     return (
@@ -56,14 +68,23 @@ export default function StartForm({ User, Color, RoomID, SetColorUser, IsDisable
                     </select>
                    
                 </div>
+                
+               {/*<div className="table-body-cell">  <button onClick={handleSubmit}>*/}
+               {/*     <span>Save in local storage</span>*/}
+               {/* </button></div>*/}
+                
+            </div >
+            <div className="resp-table-body">
+                <div className="table-body-cell">Scan qrcode to join by mobile</div>
+                <div className="table-body-cell">
+                    <img src={qrCode} alt="" />
+                </div>
                 <div className="table-body-cell">Invitation link</div>
                 <div className="table-body-cell">
                     <input type="text" name="link" value={url} />
                 </div>
-                <div className="table-body-cell">  <button onClick={handleSubmit}>
-                    <span>Save in local storage</span>
-                </button></div>
-            </div >
+                
+            </div>
             </div >
         );
    
