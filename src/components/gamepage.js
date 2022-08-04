9dffa473f455013086c900f5f778c98d3e93705f
Anton Takov <A54H0ZZ@mmm.com>
2022-08-04 10:46:15 +0300

    Fixed localstorage

--- src/components/gamepage.js
+++ src/components/gamepage.js
@@ -15,7 +15,7 @@ function GamePage() {
     const ouser = new URLSearchParams(search).get('user');
     const ocolor = new URLSearchParams(search).get('color');
     const room= new URLSearchParams(search).get('room')
-    const userinfo = JSON.parse(LocalStorage.getItem('userinfo'));
+    const userinfo = JSON.parse(localStorage.getItem('userinfo'));
     let localuser = userinfo && userinfo.username ? userinfo.username : shortid.generate().substring(0, 5);
     const localcolor = userinfo && userinfo.color ? userinfo.color : "White";
     if (localuser === ouser) localuser = localuser + shortid.generate().substring(0, 5);
